f54c4f0cb4c19e91e201df3a653eb4e7f893e0ac
Rakesh Daharwal <rakeshdaharwal786@gmail.com>
2025-05-09 17:54:42 +0530

    vite env url added

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -73,8 +73,7 @@ export default function SignUp() {
       }
 
       try {
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/sign-up`, formData);
-
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/sign-up`, formData);
 
         if (response.status === 201) {
           toast.success('OTP has been sent to your email. Please check it!');
@@ -87,7 +86,7 @@ export default function SignUp() {
     } else if (activeStep === 1) {
       if (formData.otp === '123456') {
         try {
-          await axios.post(`${process.env.REACT_APP_API_URL}/verify-email`, {
+          await axios.post(`${import.meta.env.VITE_API_URL}/verify-email`, {
             email: formData.email,
           });
 
